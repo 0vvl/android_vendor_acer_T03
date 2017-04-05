a1725e4a7184c394be7d517d07f5df3e8da7d75c
owl <filin0142@gmail.com>
2017-04-05 23:26:28 +0300

    first commit

---
+++ T03-vendor-blobs.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/acer/T03/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/acer/T03/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/acer/T03/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/acer/T03/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/acer/T03/vendor,system/vendor)
